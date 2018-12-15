94895de1951e7f3683574304bab8193444959ab1
hongtuanquyen <hongtuanquyen@gmail.com>
2018-12-15 23:03:20 +0700

    Update

--- check_dang_nhap.php
+++ check_dang_nhap.php
@@ -1,18 +1,18 @@
 <?php
-	$username_db = "kkkkkkkk";
-	$password_db = "kkkkkkkk";
+	$username_db = "htqhtqhtq";
+	$password_db = "htqhtqhtq";
 	$username = $_GET["username"];
 	$password = $_GET["pass"];
  
   
 	// Nếu thông tin đăng nhập chính xác, trả về giá trị là 1
-	if ($username == $username_db && $password == $password_db) {
+	if ($username == $username_db || $password == $password_db) {
 		echo 1;
 		exit();
 	}
  
 	// Nếu thông tin đăng nhập sai, trả về giá trị là 0
 	// echo 0;
-  echo $username;
+  echo 0;
 	exit();
 ?>
